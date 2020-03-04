d240e25f0f03999e4188c8234c784d2df56b5241
sam kim <quroom@naver.com>
2020-03-04 09:44:13 +0900

    django first commit

---
+++ onepaper/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for onepaper project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onepaper.settings')
+
+application = get_wsgi_application()
