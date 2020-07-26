3f6345d48677d15089cb85b3be8d5c320726f0c5
Sangeun Kim <quroom@naver.com>
2020-07-26 09:26:18 +0900

    Remove auto adding profile code when user is crated

--- profiles/signals.py
+++
@@ -1,9 +0,0 @@
-from profiles.models import CustomUser
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from profiles.models import Profile
-
-@receiver(post_save, sender=CustomUser)
-def create_profile(sender, instance, created, **kwargs):
-     if created:
-         Profile.objects.create(user=instance, profile_name="Default")
