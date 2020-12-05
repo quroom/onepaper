836d33d7ccdd733f13fabfb9628147100feb88ad
quroom <quroom@naver.com>
2020-12-06 07:43:07 +0900

    Change Page Size

--- onepaper/settings.py
+++ onepaper/settings.py
@@ -194,7 +194,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 4
+    'PAGE_SIZE': 6
 }
 
 WEBPACK_LOADER = {
