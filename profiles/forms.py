15b97bde3fdd8b6d6a05c44538ab19cf42d1e295
quroom <quroom@naver.com>
2020-12-20 09:44:02 +0900

    Temporary code for registration

--- profiles/forms.py
+++ profiles/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.admin.widgets import AdminDateWidget
 from django import forms
+from phonenumber_field.formfields import PhoneNumberField
 from profiles.models import CustomUser
 from django_registration.forms import RegistrationFormUniqueEmail
 from datetime import datetime
@@ -13,6 +14,10 @@ class CustomUserForm(RegistrationFormUniqueEmail):
     name = forms.CharField(label=_('이름'), required=True, max_length=150)
     birthday = forms.DateField(label=_('생년월일'), required=True, widget=forms.SelectDateWidget(years=range(this_year - date_range, this_year), attrs = {'class': 'form-control snps-inline-select'}))
     is_expert = forms.BooleanField(label=_('사업자 계정으로 등록하길 원하시면 체크.'), initial=False, required=False)
+    mobile_number = PhoneNumberField(label=_('휴대폰 번호'), required=True)
+    address = forms.CharField(label=_('주소'), required=True, max_length=250)
+    dong = forms.CharField(label=_('동'), required=False, max_length=20)
+    ho = forms.CharField(label=_('호'), required=False, max_length=20)
 
     class Meta(RegistrationFormUniqueEmail.Meta):
         model = CustomUser
