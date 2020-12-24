fb5f940597f6f189beaf4faf7f54a28f2b9e667e
quroom <quroom@naver.com>
2020-12-24 12:27:35 +0900

    Add post_save signal for updating expert profile status to REQUEST

--- profiles/signals.py
+++ profiles/signals.py
@@ -1,9 +1,15 @@
-from django.db.models.signals import post_delete
+from django.db.models.signals import post_delete, post_save
 from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
 from ipware import get_client_ip
-from profiles.models import AllowedUser, CustomUser, Profile
+from profiles.models import AllowedUser, CustomUser, ExpertProfile, Profile
 from profiles.serializers import CustomUserSerializer
+
+@receiver(post_save, sender=ExpertProfile)
+def post_expert_profile(sender, instance, **kwargs):
+    if instance.status != ExpertProfile.REQUEST:
+        instance.status = ExpertProfile.REQUEST
+        instance.save()
 
 @receiver(post_delete, sender=Profile)
 def delete_address(sender, instance, **kwargs):
