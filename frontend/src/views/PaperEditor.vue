a6b8efd2de50c1b1ccd455711854faad9ca23348
Sangeun Kim <quroom@naver.com>
2021-02-28 15:43:14 +0900

    Update editor style for quill-editor

--- frontend/src/views/PaperEditor.vue
+++ frontend/src/views/PaperEditor.vue
@@ -729,7 +729,8 @@ export default {
         modules: {
           toolbar: {
             container: [
-              ['bold', 'underline', {'list': 'ordered'}, { 'size': ['small', false, 'large', 'huge'] }],
+              [{ 'size': ['small', false, 'large', 'huge'] }, 'bold', 'underline', ],
+              [{'list': 'ordered'}, { 'align': []},],
               ['image', 'link']
             ],
             handlers: {
