df1f709de9eb5d980ebd16074f526877c06c4dee
quroom <quroom@naver.com>
2021-05-11 15:27:34 +0900

    Fix S3 media path to only media not include media-green

--- onepaper/storages.py
+++ onepaper/storages.py
@@ -2,25 +2,22 @@ from storages.backends.s3boto3 import S3Boto3Storage
 from django.conf import os
 
 __all__ = (
-    'S3StaticStorage',
-    'S3DefaultStorage',
+    "S3StaticStorage",
+    "S3DefaultStorage",
 )
 
 # for media
 class S3DefaultStorage(S3Boto3Storage):
-    default_acl = 'public-read'
-    GREEN = os.environ.get("GREEN", 'True') != 'False'
-    if GREEN:
-        location = 'media-green'
-    else:
-        location = 'media'
+    default_acl = "public-read"
+    location = "media"
     file_overwrite = False
-    
+
+
 # for static
 class S3StaticStorage(S3Boto3Storage):
-    default_acl = 'public-read'
-    GREEN = os.environ.get("GREEN", 'True') != 'False'
+    default_acl = "public-read"
+    GREEN = os.environ.get("GREEN", "True") != "False"
     if GREEN:
-        location = 'static-green'
+        location = "static-green"
     else:
-        location = 'static'
+        location = "static"
