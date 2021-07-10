bdcf3517406e223354148f1c24d4d38453692749
quroom <quroom@naver.com>
2021-07-10 18:13:50 +0900

    Add code for registration static and Fix some problems

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http.response import HttpResponseRedirect
+from django.urls import reverse
 from django.views.generic.base import TemplateView
 
 # class IntroPageView(TemplateView):
@@ -7,12 +9,27 @@ from django.views.generic.base import TemplateView
 #         return template_name
 
 class IndexTemplateView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('onepaper_intro'))
+        else:
+            context = self.get_context_data(**kwargs)
+            return self.render_to_response(context)
+
     def get_template_names(self):
         if self.request.user.is_authenticated:
             if settings.DEBUG:
                 template_name = "index-dev.html"
             else:
                 template_name = "index.html"
-        else:
-            template_name = "intro.html"
+        return template_name
+
+class IntroPageView(TemplateView):
+    def get_template_names(self):
+        template_name = "intro.html"
+        return template_name
+
+class EmailConfirmedView(TemplateView):
+    def get_template_names(self):
+        template_name = "account/email_confirmed.html"
         return template_name
