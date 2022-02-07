f26da9555aef1cf818e592de17df9313a130b33c
quroom <quroom@naver.com>
2022-02-07 10:28:15 +0900

    Change dong/ho display

--- frontend/src/views/PaperDetail.vue
+++ frontend/src/views/PaperDetail.vue
@@ -69,12 +69,12 @@
           <v-card class="pl-2" outlined tile>{{ paper.address.old_address }}</v-card>
         </v-col>
         <v-col class="text-center font-weight-bold" cols="1">
-          <v-card outlined tile>{{ $t("dong") }} / {{ $t("ho") }}</v-card>
+          <v-card outlined tile>{{ `${$t("dong")}${$t("ho")}` }}</v-card>
         </v-col>
         <v-col cols="3">
           <v-card class="pl-2" outlined tile height="100%">
             <span v-if="!!paper.address.dong"> {{ paper.address.dong }} {{ $t("dong") }} </span>
-            <span v-if="!!paper.address.ho"> {{ paper.address.ho }} {{ $t("ho") }} </span>
+            <span v-if="!!paper.address.ho"> {{ paper.address.ho }}{{ $t("ho") }} </span>
           </v-card>
         </v-col>
       </v-row>
@@ -135,7 +135,7 @@
         </v-row>
         <p
           v-if="paper.from_date && paper.to_date"
-          class="ma-0 mt-2"
+          class="my-2"
           v-html="
             $t('terms_and_conditions_period', {
               from_year: paper.from_date.split('-')[0],
