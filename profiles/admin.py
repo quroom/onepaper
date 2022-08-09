452be450420a5b4edb4667d341765a14d7626504
quroom <quroom@naver.com>
2022-08-10 04:53:28 +0900

    Update admin page list_display

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,6 +1,21 @@
 from django.apps import apps
 from django.contrib import admin
 
+from profiles.models import CustomUser, Profile
+
 # Register your models here.
 for model in apps.get_app_config("profiles").get_models():
-    admin.site.register(model)
+    if model != Profile and model != CustomUser:
+        admin.site.register(model)
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("email", "updated_at", "name", "birthday")
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "updated_at", "address", "mobile_number")
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
